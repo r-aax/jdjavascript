b1453e1995c67e1eddd9a6a445fc66198e26beba
Alexey Rybakov <rybakov@ians.aero>
2015-03-27 16:38:41 +0300

    English : test vocabulary

---
+++ Sites/English/Vocabulary.js
@@ -0,0 +1,38 @@
+// Vocabulary.js - vocabulary for english tests.
+//
+// Author: Alexey Rybakov
+
+//--------------------------------------------------------------------------------------------------
+// Create.
+//--------------------------------------------------------------------------------------------------
+
+// Create vocabulary.
+function Vocabulary()
+{
+    this.Subvocabularies = new Array();
+
+    this.Subvocabularies.push(CreateSubvocabulary_Top100());
+}
+
+//--------------------------------------------------------------------------------------------------
+// Statistic.
+//--------------------------------------------------------------------------------------------------
+
+// Get count of words in vocabulary.
+//
+// Result:
+//   Total count of words.
+Vocabulary.prototype.Count = function()
+{
+    var n = 0;
+
+    for (var i = 0; i < this.Subvocabularies.length; i++)
+    {
+        n += this.Subvocabularies[i].length;
+    }
+
+    return n;
+}
+
+//--------------------------------------------------------------------------------------------------
+
